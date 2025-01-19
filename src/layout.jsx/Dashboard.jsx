fc567a2dc29e2871838bf302d968c163e9b973fb
Rana <reduanulislam92665@gmail.com>
2025-01-19 18:06:59 +0600

    Admin home page done

--- src/layout.jsx/Dashboard.jsx
+++ src/layout.jsx/Dashboard.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 import useAuth from '../Hooks/useAuth';
 import useAxiosSecure from '../Hooks/useAxiosSecure';
+import '../CSS/Dashboard/Dashboard.css';
 
 const Dashboard = () => {
   const [user1, setUser] = useState(null); // Initialize with null
@@ -31,36 +32,36 @@ const Dashboard = () => {
         <ul className="space-y-4">
           <>
             <li className="pr-4">
-              <Link to="/">Home</Link>
+              <NavLink to="/">Home</NavLink>
             </li>
             <li className="pr-4">
-              <Link to="/dashboard">Dashboard Home</Link>
+              <NavLink to="/dashboard">Dashboard Home</NavLink>
             </li>
             <div className="divider"></div>
             {user1.role === 'admin' && (
               <li className="pr-4">
-                <Link to="allUser">All User</Link>
+                <NavLink to="allUser">All User</NavLink>
               </li>
             )}
             {(user1.role === 'admin' || user1.role === 'volunteer') && (
               <>
                 <li className="pr-4">
-                  <Link to="all-blood-donation-request">All Blood Donation Request</Link>
+                  <NavLink to="all-blood-donation-request">All Blood Donation Request</NavLink>
                 </li>
                 <li className="pr-4">
-                  <Link to="content-Management">Content Management</Link>
+                  <NavLink to="content-Management">Content Management</NavLink>
                 </li>
               </>
             )}
             <li className="pr-4">
-              <Link to="profile">Profile</Link>
+              <NavLink to="profile">Profile</NavLink>
             </li>
             <li className="pr-4">
-              <Link to="donationRequest">Create Donation Request</Link>
+              <NavLink to="donationRequest">Create Donation Request</NavLink>
             </li>
             {user1.role === 'donor' && (
               <li className="pr-4">
-                <Link to="myDonationRequest">My Donation Request</Link>
+                <NavLink to="myDonationRequest">My Donation Request</NavLink>
               </li>
             )}
           </>
--- src/pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/pages/Dashboard/AdminHome/AdminHome.jsx
@@ -1,14 +1,52 @@
 import React from 'react';
+import { useQuery } from '@tanstack/react-query';
 import useAuth from '../../../Hooks/useAuth';
+import useAxiosPublic from '../../../hooks/useAxiosPublic';
 
 const AdminHome = () => {
-      const { user } = useAuth()
-      return (
-            <div>
-                  <h2>Admin Home</h2>
-                  Welcome {user.displayName} to Blood Bank
-            </div>
-      );
+  const { user } = useAuth();
+  const axiosPublic = useAxiosPublic();
+
+  const fetchAdminState = async () => {
+    const response = await axiosPublic.get('/admin-state');
+    return response.data;
+  };
+
+  const { data: adminState, isLoading, error } = useQuery({
+    queryKey: ['adminState'],
+    queryFn: fetchAdminState
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error fetching admin state</div>;
+  }
+
+  return (
+    <div>
+      <div>
+      <h2 className='text-center text-2xl'>Admin Home</h2>
+            <p className='text-center text-xl'> Welcome {user.displayName} to Blood Bank</p>
+      </div>
+      <div className="stats shadow flex items-center justify-center">
+        <div className="stat">
+          <div className="stat-title">Total Users</div>
+          <div className="stat-value">{adminState.users}</div>
+        </div>
+        <div className="stat">
+          <div className="stat-title">Total Donation Requests</div>
+          <div className="stat-value">{adminState.donationRequest}</div>
+        </div>
+        <div className="stat">
+          <div className="stat-title">Total Revenue</div>
+          <div className="stat-value">${adminState.revenue}</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AdminHome;
