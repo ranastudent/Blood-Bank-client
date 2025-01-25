5d83eb61653fcf5cf3bb1b7d802816e76e98faef
Rana <reduanulislam92665@gmail.com>
2025-01-25 08:15:19 +0600

    Make Feature Responsive

--- src/pages/Home/Feature/Feature.jsx
+++ src/pages/Home/Feature/Feature.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Feature = () => {
       return (
-            <div className=''>
-                  <div className=''>
+            <div className='flex flex-col md:flex-row'>
+                  <div className='md:w-1/2 p-4'>
                         <h2 className="text-2xl font-bold mb-4">Why Blood Donation is Important</h2>
                         <p className="mb-4">
                               Blood donation is a vital process that helps save lives. Every two seconds, someone in the world needs blood, whether it's for surgeries, cancer treatments, chronic illnesses, or traumatic injuries. By donating blood, you can make a significant impact on the health and well-being of others.
@@ -15,8 +15,8 @@ const Feature = () => {
                               Join us in our mission to ensure a stable and safe blood supply for everyone. Your contribution can make a world of difference.
                         </p>
                   </div>
-                  <div className=''>
-                        <img className='w-full min-h-screen object-cover' src="https://cdn.vectorstock.com/i/preview-1x/78/70/banner16-9-vector-38807870.jpg" alt="Blood Donation Banner" />
+                  <div className='md:w-1/2'>
+                        <img className='w-full h-auto object-cover object-center' src="https://cdn.vectorstock.com/i/preview-1x/78/70/banner16-9-vector-38807870.jpg" alt="Blood Donation Banner" />
                   </div>
             </div>
       );
